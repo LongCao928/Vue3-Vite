578ce18367107693076a5f5ed283a5520a9c552e
LongCao928 <17512055928@163.com>
2023-04-09 16:11:45 +0800

    axios config upgrade

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,6 +5,7 @@ import qs from 'qs'
 // const baseURL = import.meta.env.VUE_APP_URL
 // const envType = import.meta.env.VUE_APP_ENV
 
+// `withCredentials` 表示跨域请求时是否需要使用凭证
 // axios.defaults.withCredentials = true
 // console.log(`${envType}：${baseURL}`)
 
@@ -19,13 +20,13 @@ function makeUrl(host, path, query) {
 }
 
 export default {
-    async get(path, data) {
+    async get(path, data, config) {
         // return axios.get('/user?id=123')
         // return axios.get('https://baidu.com' + path, { params: data })
-        return axios.get(makeUrl('https://baidu.com', path, data))
+        return axios.get(makeUrl('https://baidu.com', path, data), config ? config : {})
     },
-    async post(path, data) {
+    async post(path, data, config) {
         // return axios.post('https://baidu.com' + path, data)
-        return axios.post(makeUrl('https://baidu.com', path, null), data)
+        return axios.post(makeUrl('https://baidu.com', path, null), data, config ? config : {})
     }
 }
