4c689820b6480db027a9f6e1c38be6978972833a
LongCao928 <17512055928@163.com>
2023-04-10 16:18:31 +0800

    prettier:write

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true, // 支持浏览器环境的检测
+        "es2021": true, // 支持es2021语法的检测
+        "node": true // 支持node环境的检测
+    },
+    "extends": [
+        "eslint:recommended", // 使用eslint推荐的配置进行检测
+        "plugin:vue/vue3-essential" // 支持vue3相关语法的检测
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest", // 解析文件的时候使用最新的ecmaVersion
+        "sourceType": "module"  // 文件是ES6模块规范
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": { // 配置自己项目特有的一些检测规则
+    }
+}
